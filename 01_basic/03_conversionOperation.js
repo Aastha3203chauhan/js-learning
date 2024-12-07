75b96030ce855c38dcfac50b1e1ee851f4d8cd29
Aastha3203chauhan <164637101+Aastha3203chauhan@users.noreply.github.com>
2024-12-07 16:07:12 +0000

    conversions

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -13,13 +13,50 @@
 // "33xyz" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = 0node 01_basic/03_conversionOperation.js
+// let isLoggedIn = "Aastha"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
+//1 => true , 0 => false 
+// "" => false
+//"Aastha" => true
 
+// let someNum = 22
+// let stringNum = String(someNum)
 
- 
-//const {score} = req.body;
+// console.log(stringNum);
+// console.log(typeof(stringNum));
+
+/************ Operations **************/
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);  remainder
+
+let str1 = "Aastha"
+let str2 = " Chauhan"
+let str3 = str1 + str2 
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 300
+gameCounter++;
+console.log(gameCounter);
+
