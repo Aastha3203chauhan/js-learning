174420ece58a727ab3ef766bbff1ee9b284e839f
Aastha3203chauhan <164637101+Aastha3203chauhan@users.noreply.github.com>
2024-12-07 12:31:04 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score = null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+
+//console.log(typeof(valueInNumber));
+
+//console.log(valueInNumber); //Nan means not a number
+
+// "33" => 33
+// "33xyz" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 0node 01_basic/03_conversionOperation.js
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+
+ 
+//const {score} = req.body;
